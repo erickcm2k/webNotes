467d7bb4abbea7403af396fa86bc8ba06f949efd
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-23 19:28:54 -0500

    Game Color finished

--- webDeveloperBootcamp/17.- Game Color/script.js
+++ webDeveloperBootcamp/17.- Game Color/script.js
@@ -2,19 +2,32 @@ const squares = document.getElementsByClassName("square");
 const rgbColorLabel = document.querySelector("span");
 const message = document.getElementById("message");
 const resetButton = document.querySelector("button");
+const easyButton = document.getElementById("easy-btn");
+const hardButton = document.getElementById("hard-btn");
 
-var colors = generateRandomColors(6);
-var randomPosition = pickColor();
+easyButton.addEventListener("click", function () {
+  hardButton.classList.remove("selected");
+  easyButton.classList.add("selected");
+  easyMode();
+});
+hardButton.addEventListener("click", function () {
+  easyButton.classList.remove("selected");
+  hardButton.classList.add("selected");
+  hardMode();
+});
+
+var colors = newColors(6);
+var randomPosition = guessColorValue(5);
 var randomColor = colors[randomPosition];
 
-initializeGame();
+startGame();
 
 rgbColorLabel.textContent = `${randomColor} Hint: ${
   Number(randomPosition) + 1
 }`;
 resetButton.addEventListener("click", resetGame);
 
-function initializeGame() {
+function startGame() {
   for (var i = 0; i < squares.length; i++) {
     // Add initial colors
     squares[i].style.background = colors[i];
@@ -37,32 +50,62 @@ function initializeGame() {
 }
 
 function resetGame() {
-  colors = generateRandomColors(6);
+  if (easyButton.classList.contains("selected")) {
+    easyMode();
+  } else if (hardButton.classList.contains("selected")) {
+    hardMode();
+  } else {
+    // Hard mode by default
+    hardMode();
+  }
+}
+
+function hardMode() {
+  colors = newColors(6);
 
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
+    squares[i].style.display = "block";
   }
 
-  randomPosition = pickColor();
+  randomPosition = guessColorValue(5);
   randomColor = colors[randomPosition];
   rgbColorLabel.textContent = `${randomColor} Hint: ${
     Number(randomPosition) + 1
   }`;
 }
 
-function pickColor() {
-  return Math.round(Math.random() * 5);
+function easyMode() {
+  colors = newColors(3);
+
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+
+  randomPosition = guessColorValue(2);
+  randomColor = colors[randomPosition];
+  rgbColorLabel.textContent = `${randomColor} Hint: ${
+    Number(randomPosition) + 1
+  }`;
 }
 
-function generateRandomColors(num) {
+function guessColorValue(num) {
+  return Math.round(Math.random() * num);
+}
+
+function newColors(num) {
   let colors = [];
   for (let i = 0; i < num; i++) {
-    colors.push(getRandomColor());
+    colors.push(getRandomRGB());
   }
   return colors;
 }
 
-function getRandomColor() {
+function getRandomRGB() {
   let R = Math.round(Math.random() * 255);
   let G = Math.round(Math.random() * 255);
   let B = Math.round(Math.random() * 255);
