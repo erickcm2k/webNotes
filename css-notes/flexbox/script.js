6afb11cc7911c42eb574b9c2c1cf239860ad277a
Erick Castañeda Martínez <erickce40@gmail.com>
2021-01-04 22:04:16 -0600

    preparing for frontend masters webpack course

--- css-notes/flexbox/script.js
+++
@@ -1,7 +0,0 @@
-let words = document.getElementsByClassName('words');
-
-for(let i = 0; i < words.length; i++) {
-  words[i].addEventListener('click', function() {
-      alert(`You pressed ${i + 1}th element.`);
-  });
-}
