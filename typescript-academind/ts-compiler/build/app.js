786a961f23109e60c4262afafc43517ec513f546
Erick Castañeda Martínez <erickce40@gmail.com>
2021-07-18 15:12:32 -0500

    ts compiler

---
+++ typescript-academind/ts-compiler/build/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+    console.log("hello world!");
+});
+//# sourceMappingURL=app.js.map
---
+++ typescript-academind/ts-compiler/src/app.ts
@@ -0,0 +1,5 @@
+const button = document.getElementById("btn")!;
+
+button.addEventListener("click", () => {
+  console.log("hello world!");
+});
